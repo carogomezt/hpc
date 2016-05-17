1f5d9f99faec3274832210e5b6814ff4d7896628
hpc course <hpc@gmail.com>
2016-05-17 16:28:41 -0500

    New structure

---
+++ secVScuda/image_processing/Gray/gray_cuda/build/CMakeFiles/gray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gray.dir/./gray_generated_gray.cu.o"
+  "gray.pdb"
+  "gray"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/gray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
